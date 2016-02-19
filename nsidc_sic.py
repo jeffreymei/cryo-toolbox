9b66b1e16a5957a5573d5749b823a060edd5a8b5
jeffreymei <mym226@nyu.edu>
2016-02-19 14:11:27 -0500

    working example

--- nsidc_sic.py
+++ nsidc_sic.py
@@ -1,32 +1,34 @@
 import numpy as np
 import datetime as dt
-import struct, os
+import struct, os, commands
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
 
-def get_data_nsidc(i, ls_list):
-	year, month, day = int(ls_list[i][3:7]), int(ls_list[i][7:9]), int(ls_list[i][9:11])
+def get_data_nsidc(filename):
+	year, month, day = int(filename[3:7]), int(filename[7:9]), int(filename[9:11])
 	date = dt.date(year, month, day)
-	icefile = open(ls_list[i])
+	icefile = open(filename)
 	contents = icefile.read()
 	icefile.close()
 	# unpack binary data into a flat tuple z
-	
-	if source=="NASA":
+	source = filename[:2]
+	if source=="nt": #nasa
 		s="%dB" % (int(width*height),)
 		z=struct.unpack_from(s, contents, offset = 300)#was 300
 		nsidc = np.array(z).reshape((332,316))
-		nsidc = np.rot90(nsidc, 2)/2.5
-	elif source=="BS":
-		icefile = open(ls_list[i])
+		nsidcdata = np.rot90(nsidc, 2)/2.5
+	elif source=="bt": #bootstrap
+		icefile = open(filename)
 		contents = np.fromfile(icefile, dtype='<i2')
 		nsidc = np.array(contents).reshape((332,316))
-		nsidc = np.rot90(nsidc, 2)/10.
+		nsidcdata = np.rot90(nsidc, 2)/10.
+	else:
+		print "please cd into the directory of the file. files should start with either bt_ or nt_"
+	return nsidcdata, date
 
-	return nsidc, date
-
-def get_data_asi(i, ls_list):
-	datagroup = Dataset(ls_list[i], "r")
+def get_data_asi(filename):
+	datagroup = Dataset(filename, "r")
 	time = datagroup["time"][:][0]
 	zz = datagroup["sea_ice_area_fraction"][0]
 	lands = datagroup["land"][:]
@@ -67,7 +69,7 @@ def plot_map_asi(i, ZZ, date, show=True):
 
 	return
 
-def plot_map_nsidc(i, ZZ, date, show=True):
+def plot_map_nsidc(ZZ, date, show=True):
 	plt.clf()
 	m = Basemap(projection='spstere',boundinglat = -60, lon_0=-180,resolution='h')
 	m.drawcoastlines(linewidth=3, linestyle = '-', color='w')
@@ -76,7 +78,7 @@ def plot_map_nsidc(i, ZZ, date, show=True):
 	m.drawmeridians(np.arange(0,177,15),labels=[False,False,False,False], xoffset=1.00, yoffset=1.00)
 
 	#test if the lat/lon grid is downloaded yet, if not then download:
-	if os.path.isfile("pss25lons_v3.dat")==False:
+	if os.path.isfile("pss25lons_v3.dat")*os.path.isfile("pss25lats_v3.dat")==False:
 		commands.getoutput("wget ftp://sidads.colorado.edu/pub/DATASETS/seaice/polar-stereo/tools/pss25lats_v3.dat") #get the lat/lon grid from NSIDC
 		commands.getoutput("wget ftp://sidads.colorado.edu/pub/DATASETS/seaice/polar-stereo/tools/pss25lons_v3.dat") #get the lat/lon grid from NSIDC
 		commands.getoutput("wget ftp://sidads.colorado.edu/pub/DATASETS/seaice/polar-stereo/tools/pss25area_v3.dat") #get the lat/lon grid from NSIDC
@@ -92,8 +94,8 @@ def plot_map_nsidc(i, ZZ, date, show=True):
 	mlons, mlats = m(lons, lats)
 	XX, YY = mlons, mlats
 
-	plt.contour(XX,YY,ZZ,alpha=1, cmap = pylab.cm.jet)
-	plt.pcolormesh(XX,YY,ZZ, cmap = pylab.cm.YlGnBu, alpha=1)
+	plt.contour(XX,YY,ZZ,alpha=1, cmap = 'jet')
+	plt.pcolormesh(XX,YY,ZZ, cmap = 'YlGnBu', alpha=1)
 	plt.tight_layout( rect=[0,0.06,1,1])
 	cbar = plt.colorbar(orientation="horizontal", fraction=.03, aspect=46, pad=.07)
 	cbar.set_label("% ice cover")
