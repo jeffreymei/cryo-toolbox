1303ea1309d9c46ec7ea18381797bc9bde4d81a5
Jeffrey <mym226@nyu.edu>
2019-07-15 16:00:49 -0400

    now accepts better system arguments with help page

--- parse_nsidc.py
+++ parse_nsidc.py
@@ -1,48 +1,42 @@
 '''
-Call this with "python parse_nsidc https://directory/with/files/ ext"
+Call this with "python parse_nsidc.py -u https://directory/with/files/ -e ext"
 note the URL must end with "/", and the extension should just be 'xml', 'hdf' and not have '.'
 This will save all .ext files from the URL in folders in the current directory.
-If you don't have a .netrc file with permissions, (https://nsidc.org/support/faq/what-options-are-available-bulk-downloading-data-https-earthdata-login-enabled)
-please enter your username as a 3rd argument, e.g. 'python https://url/folder/ ext johnsmith'
+If you don't have a .netrc file with permissions, (https://nsidc.org/support/faq/what-options-are-available-bulk-downloading-data-https-earthdata-login-enabled), you will need to add a username, with --user.
 '''
 from lxml import html
 import requests
-import sys, os
-if len(sys.argv) == 3:
-	URL = str(sys.argv[1])
-	extension = str(sys.argv[2])
-	try: assert URL[-1] == '/'
-	except: raise Exception("URL must be directory")
-	try: assert len(extension)<5
-	except: "extension should be 'html', 'jpg' etc"
-elif len(sys.argv) == 4:
-	URL = str(sys.argv[1])
-	extension = str(sys.argv[2])
-	try: assert URL[-1] == '/'
-	except: raise Exception("URL must be directory")
-	try: assert len(extension)<5
-	except: "extension should be 'html', 'jpg' etc"
-	username = sys.argv[3]
-else:
-	print("Need two arguments, https://folder/that/contains/files and extensions to download, with an optional 3rd argument (username)")
-	# sys.exit()
+import sys, os, argparse
+
+parser = argparse.ArgumentParser(description="This downloads *.ext files from a NSIDC server. Make sure you have a login to EarthData, either with a .netrc file, or you can just pass --user <USERNAME> as an argument (it will prompt for a password).")
+parser.add_argument('-e', '--extension', help='Extension, e.g. xml', required=True)
+parser.add_argument('-u', '--url', help='URL where files are; must be a directory', required=True)
+parser.add_argument('-n', '--user', help='Username for Earthdata login (use this if you do not have stored cached credentials in a .netrc file)', required=False)
+args = parser.parse_args()
+
+try: assert args.url[-1] == '/'
+except: raise Exception("URL must be directory ending in /")
+try: assert len(args.extension)<5
+except: "extension should be 'html', 'jpg' etc"
 
 
 
-page = requests.get(URL)
+
+page = requests.get(args.url)
 tree = html.fromstring(page.content)
 out = tree.xpath('//td[@class="indexcolname"]//a/@href')[1:]
-filtered_items = [i for i in out if '.'+extension in i]
+filtered_items = [i for i in out if '.'+args.extension in i]
 
-print("Getting " + "."+extension + " files from " + URL)
+print("Getting " + "."+args.extension + " files from " + args.url)
 
 with open("./filenames.txt", "w") as f:
 	for item in filtered_items:
-		f.write(URL+'%s\n' % item)
+		f.write(args.url+'%s\n' % item)
 	# for item in outjpg:
 	# 	f.write('https://n5eil01u.ecs.nsidc.org/ICEBRIDGE/IODMS1B.001/2011.10.25/'+'%s\n' % item)
-if len(sys.argv) == 3:
+if len(sys.argv) == 5:
 	command = 'wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -r -l1 -np -e robots=off -i ./filenames.txt'
 else:
-	command = 'wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -r -l1 -np -e robots=off -i ./filenames.txt --http-user='+username+' --ask-password'
+	command = 'wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -r -l1 -np -e robots=off -i ./filenames.txt --http-user='+args.user+' --ask-password'
 os.system(command)
+os.remove("./filenames.txt")
